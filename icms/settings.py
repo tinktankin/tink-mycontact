5bcf852f4e40205586fa54538884df5cb197189d
Miraaj Kadam <61474583+miraajkadam@users.noreply.github.com>
2020-07-01 15:55:51 +0530

    C:/Program Files/Git/signup and /verify modelled

--- icms/settings.py
+++ icms/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "webapp/static")
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     '/webapp/static',
+# ]
 
 # Authentication Model for Users
 AUTH_USER_MODEL = 'webapp.User'
