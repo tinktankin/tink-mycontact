c007d87466edfb4a9d8cb1a321d7bf0af5e5b1a1
Miraaj Kadam <61474583+miraajkadam@users.noreply.github.com>
2020-07-03 13:13:02 +0530

    added header/footer,fixed incomplete /login

--- icms/urls.py
+++ icms/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
-from webapp.views import (SignUp, Verify, LoginUser,
-                          LogoutUser, Dashboard, ImportContacts,
-                          ForgotPassword, ResetPassword, ImportFromFile)
+from webapp.views import (SignUp, Verify, LoginUser, LogoutUser, Dashboard,
+                          ImportContacts, ForgotPassword, ResetPassword,
+                          ImportFromFile)
 
 from django.conf import settings
 from django.conf.urls.static import static
