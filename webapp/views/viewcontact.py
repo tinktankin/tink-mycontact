7e6241c1b2272f493070f604ec7caaea6fa65e55
Miraaj Kadam <61474583+miraajkadam@users.noreply.github.com>
2020-07-10 21:02:04 +0530

    modelled /viewcontact

--- webapp/views/viewcontact.py
+++ webapp/views/viewcontact.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from webapp.models import Contact
 
+
 class ViewContact(View):
     def get(self, request):
         cont_instance = Contact.objects.all()
-        return render(request, 'view.html', {'cont_instance':cont_instance})
+        return render(request, 'webapp/view.html',
+                      {'cont_instance': cont_instance})
 
     def post(self, request):
-         cont_instance = Contact.objects.all()
-         return render(request, 'view.html', {'cont_instance':cont_instance})
-        
+        cont_instance = Contact.objects.all()
+        return render(request, 'webapp/view.html',
+                      {'cont_instance': cont_instance})
