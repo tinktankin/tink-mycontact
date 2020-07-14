612c3d66a55e2b9cf7a15bcd521b4c4c53775e8d
Sudharshan V <sudarshan61kv@gmail.com>
2020-07-14 17:52:27 +0530

    new module

---
+++ webapp/models/contactform.py
@@ -0,0 +1,7 @@
+from django.db import models
+from .user import User
+
+class ContactForm(models.Model):
+    owner = models.OneToOneField(User, on_delete = models.CASCADE)
+    contact_type = models.CharField(choices=(('business', 'business'),('hr','hr'),('basic','basic')), max_length=20)
+    url = models.URLField()
