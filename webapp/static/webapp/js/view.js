cc73f959aeab103c90417acccad01f8dabb195f6
Miraaj Kadam <61474583+miraajkadam@users.noreply.github.com>
2020-07-17 13:33:19 +0530

    version 2 /viewcontact complete

--- webapp/static/webapp/js/view.js
+++ webapp/static/webapp/js/view.js
@@ -1,42 +1,17 @@
-// window.onload = function () {
-//     // hightlighting the selected
-//     var container = document.getElementById("btnContainer");
-//     var btns = document.getElementsByClassName("btn");
-//     for (var i = 0; i < btns.length; i++) {
-//         btns[i].addEventListener("click", function () {
-//             var current = document.getElementsByClassName("active");
-//             current[0].className = current[0].className.replace(" active", "");
-//             this.className += " active";
-//         });
-//     }
-// }
-
-// // Get the elements with class="column"
-// var elements = document.getElementsByClassName("card");
-
-// // Declare a loop variable
-// var i;
-
-// // List View
-// function listView() {
-
-// }
-
-// // Grid View
-// function gridView() {
-//     for (i = 0; i < elements.length; i++) {
-//         elements[i].style.width = "25%";
-//     }
-// }
-
 $(document).ready(function () {
     $('#list').click(function (event) {
         event.preventDefault();
         $('.item').addClass('list-group-item');
+        $('.view-group').css({
+            padding: '0 0'
+        });
     });
     $('#grid').click(function (event) {
         event.preventDefault();
         $('.item').removeClass('list-group-item');
         $('.item').addClass('grid-group-item');
+        $('.view-group').css({
+            padding: '0 50px'
+        });
     });
 });
