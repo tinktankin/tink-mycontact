0b4a06e1ff5963e4934aecfb3f02077cfa2e92e3
Harjeet Singh <singh.harjee98@gmail.com>
2020-07-21 01:39:37 +0530

    fixed bug in connection of contact form and add contact

--- webapp/models/contactform.py
+++ webapp/models/contactform.py
@@ -5,5 +5,5 @@ from .user import User
 class ContactForm(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     contact_type = models.CharField(choices=(
-        ('business', 'business'), ('hr', 'hr'), ('basic', 'basic')), max_length=20)
+        ('business', 'business'), ('hr', 'hr'), ('basic', 'basic')), max_length=20, primary_key=True)
     url = models.URLField(blank=True)
