2940ff7e871e5dee1fb650dffc17d98f13d1135b
Sohan Patil <46857626+sohansp24@users.noreply.github.com>
2020-07-27 09:41:07 +0530

    Added meeting Models

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from webapp.models import Contact, Group, User, Verification, ContactForm
+from webapp.models import Contact, Group, User, Verification, ContactForm, Meeting
 
 # Register your models here.
 admin.site.register(User)
@@ -8,3 +8,4 @@ admin.site.register(Verification)
 admin.site.register(Contact)
 admin.site.register(Group)
 admin.site.register(ContactForm)
+admin.site.register(Meeting)
